4b8e6273c57497e64c6c883859946db65b2eaa7b
Drj2001 <107933233+Darshanrgowda54@users.noreply.github.com>
2024-11-28 07:56:58 +0530

    mainmenu

--- mainmenu.c
+++ mainmenu.c
@@ -6,43 +6,58 @@
 #include "fileoperations.h"
 
 
-
 int mainmenu() {
     struct user *users = NULL;
     int choice;
 
     loadDataFromFile(&users, "bank_data.dat");
 
-    while (1) {
-        printf("\n.....Welcome to the Pthinks Bank.....\n");
+    while (1)
+    {
+        printf("\n..... Welcome to the Bank .....\n");
         printf("1. Admin Login\n");
         printf("2. User Login\n");
         printf("3. Exit\n");
         printf("Select an option: ");
         scanf("%d", &choice);
 
-        if (choice == 1) {
-            if (adminlogin()) {
+        switch (choice)
+        {
+        case 1:
+            if (adminlogin())
+            {
                 printf("Admin login successful.\n");
                 adminMenu(&users);
-            } else {
+            }
+            else
+            {
                 printf("Invalid admin credentials.\n");
             }
-        } if (choice == 2) {
+            break;
+
+        case 2:
+        {
             struct user *userlogin = userLogin(users);
-            if (userlogin != NULL) {
+            if (userlogin != NULL)
+            {
                 printf("User login successful.\n");
                 displayUserMenu(userlogin, users);
+            }
+            else
+            {
                 printf("Invalid user credentials.\n");
             }
-        }else if (choice == 3) {
-            saveDataToFile(users, "bank_data.dat");
-            printf("Exiting the system. Goodbye!\n");
             break;
-        } else {
+        }
+
+        case 3:
+            saveDataToFile(users, "bank_data.dat");
+            printf(" Thank you. Please visit again\n");
+            return 0;
+
+        default:
             printf("Invalid choice. Please try again.\n");
+            break;
         }
     }
-
-    return 0;
 }
