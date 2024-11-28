34e59ac0aa86068b9aaedbd9adff5c617ed72a17
Drj2001 <107933233+Darshanrgowda54@users.noreply.github.com>
2024-11-28 07:54:13 +0530

    admin.h

---
+++ admin.h
@@ -0,0 +1,22 @@
+#ifndef ADMIN_H
+#define ADMIN_H
+#include"user.h"
+
+struct Admin
+{
+    char adminID[20];
+    char adminPassword[20];
+};
+
+int adminlogin();
+void adminMenu(struct user **users);
+void createAccount(struct user **users);
+void deleteAccount(struct user **users, int accountNumber);
+void updateAccount(struct user **users, int accountNumber);
+void viewAccountDetails(struct user *user);
+void viewTransactionHistory(struct user *user);
+void sortUsersByAccountType(struct user **users);
+
+#endif // ADMIN_H
+
+
